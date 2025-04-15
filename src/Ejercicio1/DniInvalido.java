df2abc9016e59f8f676bea98e34922b136e212cb
Francisco Muslera <francisco.muslera@gmail.com>
2025-04-15 20:25:41 -0300

    Creo proyecto y primer clase DniInvalido

---
+++ src/Ejercicio1/DniInvalido.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+import java.io.IOException;
+
+public class DniInvalido extends IOException{
+	
+	public DniInvalido() {}
+	
+	@Override
+	public String getMessage() {
+		return "El DNI ingresado es Invalido";
+	}
+
+}
